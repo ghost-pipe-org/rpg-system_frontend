549a71d8df0735e8edb9294f7ed24ce1caa70a3b
annielymariah <annielyms@gmail.com>
2025-05-03 01:09:36 -0300

    fix: navbar and footer responsive

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -8,40 +8,43 @@ export const Navbar = () => {
 
   return (
     <nav
-      className={`flex justify-between items-center px-6 py-4 mb-8 ${
-        isAuthPage ? "bg-black" : "bg-black"
-      } border-b-2 border-transparent bg-clip-border`}
+      className="flex justify-between items-center px-6 py-4 mb-6 bg-black border-b-2 border-transparent bg-clip-border"
       style={{
-        borderImage: isAuthPage
-          ? undefined
-          : "linear-gradient(to right, #2E1F7A, #1C1C2A, #11C0B8) 1",
+        borderImage: "linear-gradient(to right, #2E1F7A, #1C1C2A, #11C0B8) 1",
       }}
     >
       <div className="flex items-center">
         <img src="/logo.svg" alt="Logo" className="h-8" />
       </div>
       {isAuthPage ? (
-        <div className="text-white text-lg font-prompt">
-          <img src="./menu.svg" alt="Menu Hamburguer" />
-        </div>
+        <div></div>
       ) : (
-        <div className="flex space-x-4 items-center justify-center text-white font-prompt">
-          <a href="/" className="hover:text-[#11C0B8] hover:font-bold font-light">
+        <div className="flex items-center justify-center text-white font-prompt gap-x-6">
+          <a
+            href="/"
+            className="hover:text-[#11C0B8] hover:font-bold font-light"
+          >
             início
           </a>
           <p className="font-extrabold">•</p>
-          <a href="/sessoes" className="hover:text-[#11C0B8] hover:font-bold font-light">
+          <a
+            href="/sessoes"
+            className="hover:text-[#11C0B8] hover:font-bold font-light"
+          >
             sessões
           </a>
           <p className="font-extrabold">•</p>
-          <a href="/eventos" className="hover:text-[#11C0B8] hover:font-bold font-light">
+          <a
+            href="/eventos"
+            className="hover:text-[#11C0B8] hover:font-bold font-light"
+          >
             eventos
           </a>
         </div>
       )}
-          <button>
-            <img src="./menu.svg" alt="Menu Hamburguer" />
-          </button>
+      <button>
+        <img src="./menu.svg" alt="Menu Hamburguer" className="h-8"/>
+      </button>
     </nav>
   );
 };
