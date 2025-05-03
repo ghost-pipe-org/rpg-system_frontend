aa6a324faf575ca86f1e9c4a12815af54a1e5584
annielymariah <annielyms@gmail.com>
2025-05-03 01:08:39 -0300

    feat:

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LandingPage } from "../pages/Landing/LandingPage";
 import { Register } from "../pages/Register/Register";
-
+import { Login } from "../pages/Login/Login";
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/cadastro" element={<Register />} />
-        <Route path="/login" element={<Register />} />
-        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
