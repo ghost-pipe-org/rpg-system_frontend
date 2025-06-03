d4fa86918eb437b451bcf74acafbd066e7a9449a
annielymariah <annielyms@gmail.com>
2025-06-03 18:56:07 -0300

    fix: Ajustando Title

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -4,11 +4,11 @@ interface TitleProps {
 
 export const Title = ({ name }: TitleProps) => {
   return (
-    <h1
-      className="flex justify-center items-center font-pixelsans font-medium text-white text-[2.5rem]
-               [text-shadow:_2px_0_theme(colors.indigo.800),_0_2px_theme(colors.indigo.600)]"
-    >
-      {name}
-    </h1>
+    <div className="flex justify-center w-full">
+      <h1 className="font-pixelsans font-medium text-white text-[2rem] text-center pb-5
+                    [text-shadow:_2px_0_theme(colors.indigo.800),_0_2px_theme(colors.indigo.600)]">
+        {name}
+      </h1>
+    </div>
   );
-};
+};  
